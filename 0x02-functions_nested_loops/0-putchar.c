186d3c20623941d91718b605de529b74167ca915
DETUMO <adetumo@gmail>
2022-09-17 02:01:56 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,20 @@
 #include "main.h"
-
 /**
- * main - Prints _putchar
- *
- * Description: using the function main
- * this program prints _putchar and returns 0
- * Return: return 0
+ * _putchar - check the code
+ * @c: character
+ * Return: Always 0.
  */
 int main(void)
 {
-	char putchar[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
-
-	unsigned int c;
-
-	for (c = 0; c < sizeof(putchar); c++)
-	{
-		_putchar(putchar[c]);
-	}
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
