a68d337ccad03b97e287e2952e94c9844d53768e
DETUMO <adetumo@gmail>
2022-09-17 05:41:23 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - lowercase character
+ * @c: letter created
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+
+		return (1);
+
+	else
+
+		return (0);
+
+}
