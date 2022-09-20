87ee6dba1cd8df66dbc6746d5f336fe3ae81d302
DETUMO <adetumo@gmail>
2022-09-20 09:03:26 -0700

    print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - func that prints a string in reverse
+ * @s: The string to be printed.
+ */
+
+void print_rev(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != 0; a++)
+	{
+	}
+	for (a = a - 1; a >= 0; a--)
+
+		_putchar(s[a]);
+	_putchar('\n');
+}
