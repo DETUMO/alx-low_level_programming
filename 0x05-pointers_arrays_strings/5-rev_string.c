967d79c4b81818392b4420bbbf2a70e1fe76d64d
DETUMO <adetumo@gmail>
2022-09-20 10:53:39 -0700

    rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - print string in reverse
+ * @str: character string
+ * Return: Always 0.
+ */
+
+void rev_string(char *s)
+
+{
+
+	int f, r_s;
+	char character;
+
+	for (f = '\0'; s[f] != 0; f++)
+
+	{
+
+	}
+
+	r_s = 0;
+
+	for (f = f - 1; r_s < f; r_s++)
+
+	{
+		character = s[f];
+		s[f] = s[r_s];
+		s[r_s] = character;
+		f--;
+	}
+}
